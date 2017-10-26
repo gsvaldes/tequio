b5024beb2621b342cd62c766874ff9c6c8bb1af7
Geoffrey S Valdes <valdesgs@FA-M800-A06836-2.local>
2017-10-26 17:33:15 -0500

    add users and core apps
    add dummy home page

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+
+class HomeView(View):
+    """
+    holding view 
+    """
+    def get(self, request, *args, **kwargs):
+        context = {"message": "hello tequio"}
+        return render(request, 'core/home.html', context=context)
--- tequio/settings/base.py
+++ tequio/settings/base.py
@@ -40,7 +40,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+
+
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +50,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+
+]
+
+LOCAL_APPS = [
+    'core.apps.CoreConfig',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
