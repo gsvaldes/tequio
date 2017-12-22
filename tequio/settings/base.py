b2d0b58be23c1ebe5878c9a59515fd5cb5f12477
Geoffrey S Valdes <valdesgs@FA-M800-A06836-2.local>
2017-12-22 15:47:19 -0600

    change whitenoise STATICFILES_STORAGE

--- tequio/settings/base.py
+++ tequio/settings/base.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'users.TequioUser'
 
