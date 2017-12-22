f8c23c2726775a8abb72fe534cb0b92514cb908c
Geoffrey S Valdes <valdesgs@FA-M800-A06836-2.local>
2017-12-22 15:12:14 -0600

    add SERVER_EMAIL setting

--- tequio/settings/production.py
+++ tequio/settings/production.py
@@ -16,6 +16,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 ADMINS = env('ADMINS')
+SERVER_EMAIL = env('MAILGUN_USERNAME')
 
 
 # Database
@@ -30,3 +31,51 @@ DATABASES = {
 ALLOWED_HOSTS = ['demo-tequio.herokuapp.com']
 
 INSTALLED_APPS += ['gunicorn', ]
+
+# LOGGING CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See https://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false', ],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', ],
+            'level': 'ERROR',
+            'propagate': True
+        },
+        'django.security.DisallowedHost': {
+            'level': 'ERROR',
+            'handlers': ['console', 'mail_admins', ],
+            'propagate': True
+        }
+    }
+}
