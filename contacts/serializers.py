4c02e2e0f0802cff5b00f9320fe5c1cdf05fd6f7
Geoffrey S Valdes <valdesgs@fa-m800-a06836.lan>
2018-01-31 06:10:25 -0600

    fix contact serializer for addresses and phones update
    use get when creating dict comp to provide default value when no id new item

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -17,13 +17,15 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class AddressSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Address
-        fields = '__all__'
+        fields = ('address', 'street', 'city', 'state', 'zip_code', 'country', 'url', 'id')
+        extra_kwargs = {"id": {"required": False, "read_only": False}}
 
 
 class PhoneSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Phone
-        fields = '__all__'
+        fields = ('number', 'url', 'id')
+        extra_kwargs = {"id": {"required": False, "read_only": False}}
 
 
 class EmailSerializer(serializers.HyperlinkedModelSerializer):
@@ -72,20 +74,39 @@ class ContactSerializer(serializers.HyperlinkedModelSerializer):
 
     def update(self, instance, validated_data):
         addresses_data = validated_data.pop('addresses')
+        instance_addresses_mapping = {address.id: address for address in instance.addresses.all()}
+        data_addresses_mapping = {address_data.get('id'): address_data for address_data in addresses_data}
+
         emails_data = validated_data.pop('emails')
         instance_emails_mapping = {email.id: email for email in instance.emails.all()}
-        data_emails_mapping = {email_data['id']: email_data for email_data in emails_data}
+        data_emails_mapping = {email_data.get('id'): email_data for email_data in emails_data}
+
         phones_data = validated_data.pop('phones')
-        # addresses
-        emails = instance.emails.all()
-        emails = list(emails)
-        # phones
+        instance_phones_mapping = {phone.id: phone for phone in instance.phones.all()}
+        data_phones_mapping = {phone_data.get('id'): phone_data for phone_data in phones_data}
 
         instance.name = validated_data.get('name', instance.name)
         instance.member = validated_data.get('member', instance.member)
         instance.last_updated_by = self.context['request'].user
+        instance.save()     
 
-        instance.save()
+        for address_id, address_data in data_addresses_mapping.items():
+            address = instance_addresses_mapping.get(address_id, None)
+            if address is None:
+                new_address = Address.objects.create(**address_data)
+                instance.addresses.add(new_address)
+            else:
+                address.address = address_data.get('address', address.address)
+                address.street = address_data.get('street', address.street)
+                address.city = address_data.get('city', address.city)
+                address.state = address_data.get('state', address.state)
+                address.zip_code = address_data.get('zip_code', address.zip_code)
+                address.country = address_data.get('country', address.country)
+                address.save()
+
+        for address_id, address in instance_addresses_mapping.items():
+            if address_id not in data_addresses_mapping:
+                address.delete()
 
         for email_id, email_data in data_emails_mapping.items():
             email = instance_emails_mapping.get(email_id, None)
@@ -99,59 +120,19 @@ class ContactSerializer(serializers.HyperlinkedModelSerializer):
         for email_id, email in instance_emails_mapping.items():
             if email_id not in data_emails_mapping:
                 email.delete()
-                
 
-        
-
-        for address_data in addresses_data:
-            url = address_data.get('url')
-            if url:
-                try:
-                    address = Address.objects.get(url=url)
-                except Address.DoesNotExist:
-                    raise TequioException('address url: {0} does not exist'.format(url))
-                address.address = address_data.get('address', address.address)
-                address.street = address_data.get('street', address.street)
-                address.city = address_data.get('city', address.city)
-                address.state = address_data.get('state', address.state)
-                address.zip_code = address_data.get('zip_code', address.zip_code)
-                address.country = address_data.get('country', address.country)
-                address.save()
+        for phone_id, phone_data in data_phones_mapping.items():
+            phone = instance_phones_mapping.get(phone_id, None)
+            if phone is None:
+                new_phone = Phone.objects.create(**phone_data)
+                instance.phones.add(new_phone)
             else:
-                address = Address.objects.create(**address_data)
-                instance.addresses.add(address)
-            
-        # for email_data in emails_data:
-        #     # for email_data in emails_data:
-        #     #     email = emails.pop(0)
-        #     #     email.email = email_data.get('email', email.email)
-        #     #     email.save
-        #     # import pdb; pdb.set_trace()
-        #     id = email_data.get('id')
-        #     if id:
-        #         try:
-        #             email = Email.objects.get(id=id)
-        #         except Email.DoesNotExist:
-        #             raise TequioException('email id: {0} does not exist'.format(id))
-        #         email.email = email_data.get('email', email.email)
-        #         email.save()
-        #     else:
-        #         email = Email.objects.create(**email_data)
-        #         instance.emails.add(email)
-
-        for phone_data in phones_data:
-            url = phone_data.get('url')
-            if url:
-                try:
-                    phone = Phone.objects.get(url=url)
-                except Phone.DoesNotExist:
-                    raise TequioException('phone url: {0} does not exist'.format(url))
                 phone.number = phone_data.get('number', phone.number)
                 phone.save()
-            else:
-                phone = Phone.objects.create(**phone_data)
-                instance.phones.add(phone)
 
+        for phone_id, phone in instance_phones_mapping.items():
+            if phone_id not in data_phones_mapping:
+                phone.delete()
 
         return instance
 
