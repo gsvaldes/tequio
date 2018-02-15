a597664bf4b16550433fb549980f36819d37e253
Geoffrey S Valdes <valdesgs@fa-m800-a06836.lan>
2018-02-15 06:31:40 -0600

    add detail js and template

---
+++ src/js/contacts/detail/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      Hello detail
+      {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Initial detail setup"
+    };
+  }
+};
+</script>
+<style>
+
+</style>
+
+
---
+++ src/js/contacts/detail/detail-main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+
+// Vue.prototype.vue_data = window.vue_data;
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
